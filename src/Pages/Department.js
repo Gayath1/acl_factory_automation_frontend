9109abc7d1ab8fbd09691ae437e65c62e8e1ff5f
Gayath1 <chandulagayan@gmail.com>
2021-07-19 16:44:21 +0530

    department create API connect

--- src/Pages/Department.js
+++ src/Pages/Department.js
@@ -2,27 +2,49 @@ import React, {useEffect,useState} from 'react';
 import "../assets/css/Usercreate.css";
 import Sidebar from "../components/sidebar/Sidebar";
 import TopNav from "../components/topnav/TopNav";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from "../components/table/Table";
 import { makeStyles } from '@material-ui/core/styles';
+import axios from "axios";
+import {Alert, AlertTitle} from "@material-ui/lab";
 
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
+
+const fields = [
+    "firstName",
+    "email",
+    "epfNo",
+    "Status"
+]
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    {
+        "id": 1,
+        "firstName": "mujeeb",
+        "lastName": "singham",
+        "email": "chandulagayan@gmail.com",
+        "verificationtoken": "1234",
+        "epfNo": null,
+        "phoneNo": "0776465645",
+        "image": null,
+        "statusId": 1,
+        "password": "$2y$10$zrrjILLqTKyxYiR3jrOdvuaE.tEG3U148gVPoe7zYQLpitytXpyU2 ",
+        "createdAt": "2021-07-16T10:38:11.002Z",
+        "updatedAt": "2021-07-16T10:38:11.002Z",
+    },
+    {
+        "id": 9,
+        "firstName": "Gayath",
+        "lastName": "Chandula",
+        "email": "chandulagayan1@gmail.com",
+        "verificationtoken": "g96wx6",
+        "epfNo": "47586598",
+        "phoneNo": null,
+        "image": "uploads/dashboard.JPG-1626512057383.jpeg",
+        "statusId": 50,
+        "password": "$2b$10$vqy4Pln0C.V88NOCdpOOFOKZYHbVGWv.yV/7XLn7cpYxLQnV2PzPi",
+    }
 ];
+
 
 const useStyles = makeStyles({
     table: {
@@ -30,53 +52,51 @@ const useStyles = makeStyles({
     },
 });
 
+const renderOrderHead = (item, index) => (
+    <th key={index}>{item}</th>
+)
+const renderOrderBody = (item, index) => (
+    <tr key={index}>
+        <td>{item.firstName}</td>
+        <td>{item.email}</td>
+        <td>{item.epfNo}</td>
+        <td>
+            <button className="usertblbutton" >Delete</button>
+        </td>
+    </tr>
+)
 
 const Department = () => {
     const classes = useStyles();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [type, setType] = React.useState('Management')
-    const [image,setImage]= ('');
-    const [selectedFile, setSelectedFile] = useState();
-    const [preview, setPreview] = useState();
+    const [departmentName, setdepartmentName] = useState("");
+    const [err, setErr] = useState("");
+    const token = localStorage.getItem("Token")
 
-    // create a preview as a side effect, whenever selected file is changed
-    useEffect(() => {
-        if (!selectedFile) {
-            setPreview(undefined)
-            return
+    const headers = {
+        headers: {
+
+            "Authorization":`Bearer ${token}`
         }
-
-        const objectUrl = URL.createObjectURL(selectedFile)
-        setPreview(objectUrl)
-
-        // free memory when ever this component is unmounted
-        return () => URL.revokeObjectURL(objectUrl)
-    }, [selectedFile])
+    };
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
-    const handleChange = (event) => {
-        setType(event.target.value);
-    };
-    const imagehandleChange = (event) => {
-        setImage(event.target.files[0]);
-    };
-
-    function handleSubmit(event) {
-        event.preventDefault();
+        return departmentName.length > 0;
     }
 
-    const onSelectFile = e => {
-        if (!e.target.files || e.target.files.length === 0) {
-            setSelectedFile(undefined)
-            return
+    const submit = async (e) => {
+        e.preventDefault();
+        setErr("");
+        try{
+
+            const body = {departmentName};
+            const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/department/1/create",body,headers);
+            window.location.reload();
+
+        } catch(err) {
+            err.response.data.message && setErr(err.response.data.message)
         }
 
-        // I've kept this example simple by using the first image instead of multiple
-        setSelectedFile(e.target.files[0])
-    }
+    };
 
     return (
         <>
@@ -89,13 +109,18 @@ const Department = () => {
                         <div className="col-6">
                             <div className="card full-height">
                                 <div>
-
+                                    {err ? (
+                                        <Alert severity="error">
+                                            <AlertTitle>Error</AlertTitle>
+                                            {err}
+                                        </Alert>
+                                    ) : null}
                                     <div className="rowuser">
                                         <label>Department Name</label>
-                                        <input type="text" autoFocus placeholder="department name"  />
+                                        <input type="text" autoFocus placeholder="department name" value={departmentName} onChange={(e) => setdepartmentName(e.target.value)} />
                                     </div>
                                     <div id="button" className="rowuser">
-                                        <button   onClick={handleSubmit}>submit</button>
+                                        <button disabled={!validateForm()}  onClick={submit}>submit</button>
                                     </div>
 
 
@@ -107,34 +132,13 @@ const Department = () => {
                     <div className="row">
                         <div className="col-12">
                             <div className="card full-height">
-                                <TableContainer component={Paper}>
-                                    <Table className={classes.table} aria-label="simple table">
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>First name</TableCell>
-                                                <TableCell align="center">Email</TableCell>
-                                                <TableCell align="center">Epf no</TableCell>
-                                                <TableCell align="center">Mobile</TableCell>
-                                                <TableCell align="center">Type</TableCell>
-                                                <TableCell align="center"></TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {rows.map((row) => (
-                                                <TableRow key={row.name}>
-                                                    <TableCell component="th" scope="row">
-                                                        {row.name}
-                                                    </TableCell>
-                                                    <TableCell align="center">{row.calories}</TableCell>
-                                                    <TableCell align="center">{row.fat}</TableCell>
-                                                    <TableCell align="center">{row.carbs}</TableCell>
-                                                    <TableCell align="center">{row.protein}</TableCell>
-                                                    <TableCell align="center"><button className="usertblbutton"  onClick={handleSubmit}>Delete</button></TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
+                                    <Table
+                                        limit="5"
+                                        headData={fields}
+                                        renderHead={(item, index) => renderOrderHead(item, index)}
+                                        bodyData={rows}
+                                        renderBody={(item, index) => renderOrderBody(item, index)}
+                                    />
                             </div>
                         </div>
                     </div>
