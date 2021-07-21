1bc7ef3aa4f247259dac3b7c3011832bbc81167c
Gayath1 <chandulagayan@gmail.com>
2021-07-21 12:58:47 +0530

    fault reason delete function

--- src/Pages/FaultReason.js
+++ src/Pages/FaultReason.js
@@ -26,6 +26,30 @@ const useStyles = makeStyles({
     },
 });
 
+const token = localStorage.getItem("Token")
+
+const headers = {
+    headers: {
+
+        "Authorization":`Bearer ${token}`
+    }
+};
+
+
+const submitdelete = async (id) => {
+
+    try{
+
+        const body = {id};
+        const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/faultreason/1/delete",body,headers);
+        window.location.reload();
+
+    } catch(err) {
+        console.log(err)
+    }
+
+};
+
 const renderOrderHead = (item, index) => (
     <th key={index}>{item}</th>
 )
@@ -37,7 +61,7 @@ const renderOrderBody = (item, index) => (
         <td>{item.faultreason}</td>
         <td>{moment(item.createdAt).format("MMM Do YY")}</td>
         <td>
-            <button className="usertblbutton" >Delete</button>
+            <button onClick={()=>{submitdelete(item.id)}} className="usertblbutton" >Delete</button>
         </td>
     </tr>
 )
