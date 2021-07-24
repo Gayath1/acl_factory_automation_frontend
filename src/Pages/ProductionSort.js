7524362372acc92401ae87e95afcbdaafa501923
Gayath1 <chandulagayan@gmail.com>
2021-07-24 11:53:32 +0530

    filter array

--- src/Pages/ProductionSort.js
+++ src/Pages/ProductionSort.js
@@ -21,11 +21,16 @@ function createData(name,empty) {
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    {
+        "po": 1,
+        "Shift": "A",
+        "Product Line": "1",
+    },
+    {
+        "po": 9,
+        "Shift": "B",
+        "Product Line": "2",
+    }
 ];
 
 const useStyles = makeStyles({
