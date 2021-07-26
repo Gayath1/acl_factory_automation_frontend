52a84706dc10e986046e376800f0ffb76eeae328
Gayath1 <chandulagayan@gmail.com>
2021-07-26 19:39:27 +0530

    product order view

--- src/Pages/ProductCard.js
+++ src/Pages/ProductCard.js
@@ -8,10 +8,123 @@ import {css} from "@emotion/css";
 import axios from "axios";
 import {HashLoader} from "react-spinners";
 import {Alert, AlertTitle} from "@material-ui/lab";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Table from "../components/table/Table";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import moment from "moment";
+import Badge from "../components/badge/Badge";
 
+const fields = [
+    "Id",
+    "Production order code",
+    "Production Code",
+    "Quantity",
+    "Status",
+    "Created At",
+    "Action"
+]
 
+const orderStatus = {
+    "50": "warning",
+    "1": "success",
+}
+const orderStatusname = {
+    "50": "Pending",
+    "1": "Finish",
+}
 
+const rows = [
+    {
+        "id": 1,
+        "firstName": "mujeeb",
+        "lastName": "singham",
+        "email": "chandulagayan@gmail.com",
+        "verificationtoken": "1234",
+        "epfNo": null,
+        "phoneNo": "0776465645",
+        "image": null,
+        "statusId": 1,
+        "password": "$2y$10$zrrjILLqTKyxYiR3jrOdvuaE.tEG3U148gVPoe7zYQLpitytXpyU2 ",
+        "createdAt": "2021-07-16T10:38:11.002Z",
+        "updatedAt": "2021-07-16T10:38:11.002Z",
+    },
+    {
+        "id": 9,
+        "firstName": "Gayath",
+        "lastName": "Chandula",
+        "email": "chandulagayan1@gmail.com",
+        "verificationtoken": "g96wx6",
+        "epfNo": "47586598",
+        "phoneNo": null,
+        "image": "uploads/dashboard.JPG-1626512057383.jpeg",
+        "statusId": 50,
+        "password": "$2b$10$vqy4Pln0C.V88NOCdpOOFOKZYHbVGWv.yV/7XLn7cpYxLQnV2PzPi",
+    }
+];
+const renderOrderHead = (item, index) => (
+    <th key={index}>{item}</th>
+)
 
+const renderOrderBody = (item, index) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.productionorderCode}</td>
+        <td>{item.productCode}</td>
+        <td>{item.orderQuantity}</td>
+        <td>
+            <Badge type={orderStatus[item.statusId]} content={orderStatusname[item.statusId]}/>
+        </td>
+        <td>{moment(item.createdAt).format("MMM Do YY")}</td>
+        <td>
+            {/*<button onClick={()=>{submitdelete(item.id)}} className="usertblbutton" >Delete</button>*/}
+        </td>
+    </tr>
+)
+
+const renderOrderBody1 = (item, index) => (
+    <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.productionorderCode}</td>
+        <td>{item.productCode}</td>
+        <td>{item.orderQuantity}</td>
+        <td>
+            <Badge type={orderStatus[item.statusId]} content={orderStatusname[item.statusId]}/>
+        </td>
+        <td>{moment(item.createdAt).format("MMM Do YY")}</td>
+        <td>
+            {/*<button onClick={()=>{active(item.productlineId,item.uuid)}} className="usertblactivebutton" >Activate</button>*/}
+        </td>
+    </tr>
+)
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 const Info = () => {
 
@@ -22,7 +135,10 @@ const Info = () => {
     const [productId,setproductId] = useState("");
     let [loading, setLoading] = useState(true);
     const [err, setErr] = useState("");
+    const [value, setValue] = React.useState(0);
     const [listData, setListData] = useState({ lists: [] });
+    const [listData1, setListData1] = useState({ lists: [] });
+    const [listData2, setListData2] = useState({ lists: [] });
     const token = localStorage.getItem("Token")
     const [unitofMeasurement,setunitofMeasurement] = useState('kg');
     const headers = {
@@ -38,6 +154,14 @@ const Info = () => {
                 `https://acl-automation.herokuapp.com/api/v1/productinfo/1/getall`,headers
             );
             setListData({lists:result.data.data.ProductDetails})
+            const result1 = await axios(
+                `https://acl-automation.herokuapp.com/api/v1/ProductionOrderscontroller/1/getall`,headers
+            );
+            setListData1({lists:result1.data.data.productionOrders})
+            const result2 = await axios(
+                `https://acl-automation.herokuapp.com/api/v1/ProductionOrderscontroller/1/getallpending`,headers
+            );
+            setListData2({lists:result2.data.data.productionOrders})
             setLoading(false);
         };
         fetchData();
@@ -61,7 +185,9 @@ const Info = () => {
         }
 
     };
-
+    const handletab = (event, newValue) => {
+        setValue(newValue);
+    };
     const  handleChange = (newValue: any, actionMeta: any) => {
         setproductId(newValue.value)
         setName(newValue.name)
@@ -181,6 +307,42 @@ const Info = () => {
                                             <button   onClick={submit}>submit</button>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="card full-height">
+                                    <div className="card__header">
+                                        <AppBar position="static" style={{background: `linear-gradient(90deg, #06518C 0%, #62B4FF 97.85%)` ,borderRadius:"8px"}}>
+                                            <Tabs TabIndicatorProps={{
+                                                style: {
+                                                    backgroundColor: "#ffffff"
+                                                }
+                                            }} value={value} onChange={handletab}  >
+                                                <Tab label="Production orders" {...a11yProps(0)} />
+                                                <Tab label="Pending" {...a11yProps(1)} />
+                                            </Tabs>
+                                        </AppBar>
+                                    </div>
+                                    <TabPanel value={value} index={0}>
+                                        <Table
+                                            limit="5"
+                                            headData={fields}
+                                            renderHead={(item, index) => renderOrderHead(item, index)}
+                                            bodyData={listData1.lists}
+                                            renderBody={(item, index) => renderOrderBody(item, index)}
+                                        />
+                                    </TabPanel>
+                                    <TabPanel value={value} index={1}>
+                                        <Table
+                                            limit="5"
+                                            headData={fields}
+                                            renderHead={(item, index) => renderOrderHead(item, index)}
+                                            bodyData={listData2.lists}
+                                            renderBody={(item, index) => renderOrderBody1(item, index)}
+                                        />
+                                    </TabPanel>
                                 </div>
                             </div>
                         </div>
