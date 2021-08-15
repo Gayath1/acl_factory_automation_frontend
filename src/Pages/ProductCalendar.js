14eba18039ad80e3ac2c3c3d6ae86bb6e80728e9
Gayath1 <chandulagayan@gmail.com>
2021-08-15 10:30:03 +0530

    change

--- src/Pages/ProductCalendar.js
+++ src/Pages/ProductCalendar.js
@@ -3,11 +3,12 @@ import "../assets/css/Usercreate.css";
 import Sidebar from "../components/sidebar/Sidebar";
 import TopNav from "../components/topnav/TopNav";
 import UserContext from "../userContext";
-import { Calendar, momentLocalizer} from 'react-big-calendar'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import axios from "axios";
 import {HashLoader} from "react-spinners";
+
 const rows = [
     {
         "id": 1,
@@ -42,29 +43,29 @@ const ProductCalendar = () => {
 
     const {userData} = useContext(UserContext);
     const localizer = momentLocalizer(moment);
-    const [listData, setListData] = useState({ lists: [] });
+    const [listData, setListData] = useState({lists: []});
     const [loading, setLoading] = useState(true);
     const token = localStorage.getItem("Token")
 
     const headers = {
         headers: {
 
-            "Authorization":`Bearer ${token}`
+            "Authorization": `Bearer ${token}`
         }
     };
 
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(
-                `https://acl-automation.herokuapp.com/api/v1/ProductionOrderscontroller/1/getallpending`,headers
+                `https://acl-automation.herokuapp.com/api/v1/ProductionOrderscontroller/1/getallpending`, headers
             );
-            setListData({lists:result.data.data.productionOrders})
+            setListData({lists: result.data.data.productionOrders})
             setLoading(false);
         };
         fetchData();
     }, [])
 
-    const events= listData.lists.map((appointment)=>{
+    const events = listData.lists.map((appointment) => {
         return {
             id: appointment.id,
             title: appointment.productInfos.productName,
@@ -76,38 +77,47 @@ const ProductCalendar = () => {
 
     if (loading) {
         return (
-            <div style={{ padding: "10px 20px", textAlign: "center", justifyContent:"center", display:"flex", alignItems:"center", width:"100%", height:"100vh", backgroundColor:"#FFFFFF"}}>
-                <HashLoader  loading={loading}  size={150} />
+            <div style={{
+                padding: "10px 20px",
+                textAlign: "center",
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#FFFFFF"
+            }}>
+                <HashLoader loading={loading} size={150}/>
             </div>
         )
     }
     return (
         <>
-            {userData.role === 70? (
+            {userData.role === 70 ? (
                 <>
-                  <Sidebar/>
+                    <Sidebar/>
                     <div id="main" className="layout__content">
                         <TopNav/>
-                            <div className="layout__content-main">
-                                <h2 className="page-header">Product Calendar</h2>
-                                <div className="row">
-                                    <div className="col-12">
-                                        <div className="card full-height">
-                                            <Calendar
-                                                localizer={localizer}
-                                                events={events}
-                                                startAccessor='start'
-                                                endAccessor='end'
-                                                views={['month', 'day', 'week']}
-                                                style={{height: 500}}
-                                            />
-                                        </div>
+                        <div className="layout__content-main">
+                            <h2 className="page-header">Product Calendar</h2>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="card full-height">
+                                        <Calendar
+                                            localizer={localizer}
+                                            events={events}
+                                            startAccessor='start'
+                                            endAccessor='end'
+                                            views={['month', 'day', 'week']}
+                                            style={{height: 500}}
+                                        />
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </>
-            ):null}
+            ) : null}
         </>
     )
 }
