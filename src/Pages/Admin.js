4779474ac74fb10e2650cbaa55a34eb0bf5d21f6
Gayath1 <chandulagayan@gmail.com>
2021-08-15 10:29:52 +0530

    change

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -11,10 +11,10 @@ import avatar from '../assets/default.png';
 import UserContext from "../userContext";
 
 const fields = [
-        "firstName",
-        "email",
-        "epfNo",
-        "Status"
+    "firstName",
+    "email",
+    "epfNo",
+    "Status"
 ]
 
 
@@ -58,19 +58,19 @@ const token = localStorage.getItem("Token")
 const headers = {
     headers: {
 
-        "Authorization":`Bearer ${token}`
+        "Authorization": `Bearer ${token}`
     }
 };
 
 const deletedata = async (id) => {
 
-    try{
+    try {
 
         const body = {id};
-        const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/admin/1/delete",body,headers);
+        const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/admin/1/delete", body, headers);
         window.location.reload();
 
-    } catch(err) {
+    } catch (err) {
         console.log(err);
     }
 
@@ -86,7 +86,10 @@ const renderOrderBody = (item, index) => (
         <td>{item.email}</td>
         <td>{item.epfNo}</td>
         <td>
-            <button onClick={()=>{deletedata(item.id)}} className="usertblbutton" >Delete</button>
+            <button onClick={() => {
+                deletedata(item.id)
+            }} className="usertblbutton">Delete
+            </button>
         </td>
     </tr>
 )
@@ -99,21 +102,21 @@ const Usercreate = () => {
     const [email, setEmail] = useState("");
     const [firstName, setfirstName] = useState("");
     const [lastName, setlastName] = useState("");
-    const [mobile,setMobile] = useState("");
-    const [epfNo,setEpfNo] = useState("");
-    const [permission,setPermission] = useState("")
+    const [mobile, setMobile] = useState("");
+    const [epfNo, setEpfNo] = useState("");
+    const [permission, setPermission] = useState("")
     const [selectedFile, setSelectedFile] = useState();
     const [image, setImage] = useState(avatar);
     const [preview, setPreview] = useState();
     const [err, setErr] = useState("");
-    const [listData, setListData] = useState({ lists: [] });
+    const [listData, setListData] = useState({lists: []});
     let [loading, setLoading] = useState(true);
     const token = localStorage.getItem("Token")
 
     const headers = {
         headers: {
 
-            "Authorization":`Bearer ${token}`
+            "Authorization": `Bearer ${token}`
         }
     };
 
@@ -136,25 +139,25 @@ const Usercreate = () => {
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(
-                `https://acl-automation.herokuapp.com/api/v1/admin/1/getall`,headers
+                `https://acl-automation.herokuapp.com/api/v1/admin/1/getall`, headers
             );
-            setListData({lists:result.data.data.organizationUser[0].users})
+            setListData({lists: result.data.data.organizationUser[0].users})
             setLoading(false);
         };
         fetchData();
     }, [])
 
     function validateForm() {
-        return email.length > 0 ;
+        return email.length > 0;
     }
 
     const submit = async (e) => {
         e.preventDefault();
         setErr("");
-        try{
-            const  formData = new FormData()
+        try {
+            const formData = new FormData()
 
-            formData.append('image',selectedFile)
+            formData.append('image', selectedFile)
             formData.append("email", email);
             formData.append("firstName", firstName);
             formData.append("lastName", lastName);
@@ -162,18 +165,18 @@ const Usercreate = () => {
             formData.append("epfNo", epfNo);
             formData.append("permission", permission);
             // const body = {email, firstName,lastName,mobile,epfNo,permission};
-            const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/admin/1/create",formData
-                ,{
+            const loginResponse = await axios.post("https://acl-automation.herokuapp.com/api/v1/admin/1/create", formData
+                , {
                     //body: formData,
-                    headers:{
+                    headers: {
                         'Accept': 'multipart/form-data',
-                         "Authorization":`Bearer ${token}`
+                        "Authorization": `Bearer ${token}`
                     },
                     //credentials: 'include',
                 });
             window.location.reload();
 
-        } catch(err) {
+        } catch (err) {
             err.response.data.message && setErr(err.response.data.message)
         }
 
@@ -191,100 +194,118 @@ const Usercreate = () => {
 
     if (loading) {
         return (
-            <div style={{ padding: "10px 20px", textAlign: "center", justifyContent:"center", display:"flex", alignItems:"center", width:"100%", height:"100vh", backgroundColor:"#FFFFFF"}}>
-                <HashLoader  loading={loading}  size={150} />
+            <div style={{
+                padding: "10px 20px",
+                textAlign: "center",
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#FFFFFF"
+            }}>
+                <HashLoader loading={loading} size={150}/>
             </div>
         )
     }
     return (
 
         <>
-            {userData.role === 1? (
-            <>
-            <Sidebar/>
-            <div id="main" className="layout__content">
-                <TopNav/>
-                <div className="layout__content-main">
-                    <h2 className="page-header">Admin</h2>
-                    <div className="row">
-                        <div className="col-6">
-                            <div className="card full-height">
-                                <div>
-                                    {err ? (
-                                        <Alert severity="error">
-                                            <AlertTitle>Error</AlertTitle>
-                                            {err}
-                                        </Alert>
-                                    ) : null}
-                                    <div className="rowuser">
-                                        <label>First Name</label>
-                                        <input type="text" autoFocus placeholder="enter your firstname" value={firstName}  onChange={(e) => setfirstName(e.target.value)} />
-                                    </div>
-                                    <div className="rowuser">
-                                        <label>Last Name</label>
-                                        <input type="text" placeholder="enter your lastname" value={lastName} onChange={(e) => setlastName(e.target.value)}/>
-                                    </div>
-                                    <div className="rowuser">
-                                        <label>Email</label>
-                                        <input type="email" id="email" pattern=".+@globex\.com"  placeholder="enter your email" value={email}  onChange={(e) => setEmail(e.target.value)} required/>
-                                    </div>
-                                    <div className="rowuser">
-                                        <label>Epf No</label>
-                                        <input type="number" placeholder="enter your epf no" value={epfNo} onChange={(e) => setEpfNo(e.target.value)}/>
-                                    </div>
-                                    <div className="rowuser">
-                                        <label>Mobile</label>
-                                        <input type="mobile" placeholder="enter your mobile no" value={mobile} onChange={(e) => setMobile(e.target.value)}/>
-                                    </div>
-                                    <div className="rowuser">
-                                        <label>Permission</label>
-                                        <select id="department" name="department" value={permission} onChange={(e) => setPermission(e.target.value)}>
-                                            <option value=""  selected></option>
-                                            <option value="1">Admin</option>
-                                            <option value="50">Associate-Admin</option>
-                                        </select>
-                                    </div>
+            {userData.role === 1 ? (
+                <>
+                    <Sidebar/>
+                    <div id="main" className="layout__content">
+                        <TopNav/>
+                        <div className="layout__content-main">
+                            <h2 className="page-header">Admin</h2>
+                            <div className="row">
+                                <div className="col-6">
+                                    <div className="card full-height">
+                                        <div>
+                                            {err ? (
+                                                <Alert severity="error">
+                                                    <AlertTitle>Error</AlertTitle>
+                                                    {err}
+                                                </Alert>
+                                            ) : null}
+                                            <div className="rowuser">
+                                                <label>First Name</label>
+                                                <input type="text" autoFocus placeholder="enter your firstname"
+                                                       value={firstName}
+                                                       onChange={(e) => setfirstName(e.target.value)}/>
+                                            </div>
+                                            <div className="rowuser">
+                                                <label>Last Name</label>
+                                                <input type="text" placeholder="enter your lastname" value={lastName}
+                                                       onChange={(e) => setlastName(e.target.value)}/>
+                                            </div>
+                                            <div className="rowuser">
+                                                <label>Email</label>
+                                                <input type="email" id="email" pattern=".+@globex\.com"
+                                                       placeholder="enter your email" value={email}
+                                                       onChange={(e) => setEmail(e.target.value)} required/>
+                                            </div>
+                                            <div className="rowuser">
+                                                <label>Epf No</label>
+                                                <input type="number" placeholder="enter your epf no" value={epfNo}
+                                                       onChange={(e) => setEpfNo(e.target.value)}/>
+                                            </div>
+                                            <div className="rowuser">
+                                                <label>Mobile</label>
+                                                <input type="mobile" placeholder="enter your mobile no" value={mobile}
+                                                       onChange={(e) => setMobile(e.target.value)}/>
+                                            </div>
+                                            <div className="rowuser">
+                                                <label>Permission</label>
+                                                <select id="department" name="department" value={permission}
+                                                        onChange={(e) => setPermission(e.target.value)}>
+                                                    <option value="" selected></option>
+                                                    <option value="1">Admin</option>
+                                                    <option value="50">Associate-Admin</option>
+                                                </select>
+                                            </div>
 
-                                    <div id="button" className="rowuser">
-                                        <button  disabled={!validateForm()} onClick={submit}>Register</button>
+                                            <div id="button" className="rowuser">
+                                                <button disabled={!validateForm()} onClick={submit}>Register</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="card full-height">
+                                        <div>
+                                            <div className="rowuser">
+                                                <label>User Image</label>
+                                                <input type='file' onChange={onSelectFile}
+                                                       placeholder="select your Image"/>
+                                            </div>
+                                            {/*<div className="rowuser">*/}
+                                            <div>
+                                                {selectedFile && <img className="form-imguser" src={preview}/>}
+                                            </div>
+                                            {/*</div>*/}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="card full-height">
+                                        <Table
+                                            limit="5"
+                                            headData={fields}
+                                            renderHead={(item, index) => renderOrderHead(item, index)}
+                                            bodyData={listData.lists}
+                                            renderBody={(item, index) => renderOrderBody(item, index)}
+                                        />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6">
-                            <div className="card full-height">
-                                <div>
-                                    <div className="rowuser">
-                                        <label>User Image</label>
-                                        <input type='file' onChange={onSelectFile} placeholder="select your Image" />
-                                    </div>
-                                    {/*<div className="rowuser">*/}
-                                    <div >
-                                        {selectedFile &&  <img className="form-imguser"  src={preview} /> }
-                                    </div>
-                                    {/*</div>*/}
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="card full-height">
-                                <Table
-                                    limit="5"
-                                    headData={fields}
-                                    renderHead={(item, index) => renderOrderHead(item, index)}
-                                    bodyData={listData.lists}
-                                    renderBody={(item, index) => renderOrderBody(item, index)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-           </>
-            ):null}
-            </>
+                </>
+            ) : null}
+        </>
 
     )
 }
