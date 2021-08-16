fe5425f904a48e1a130e8e3f12df4a6d100dfd8f
Gayath1 <chandulagayan@gmail.com>
2021-08-16 17:03:18 +0530

    change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'
 import "../assets/css/Home.css";
 import welcome from '../assets/welcome.gif'
@@ -11,25 +11,25 @@ const Home = () => {
         <>
             <div className='form-containerhome'>
                 <div className='form-content-lefthome'>
-                    <img className='form-imghome' src={welcome} alt='spaceship' />
+                    <img className='form-imghome' src={welcome} alt='spaceship'/>
                 </div>
 
                 <div className='form-content-right'>
                     <div className="btnhome">
                         <Link to="/Loginadmin">
-                    <button className="form-input-btnhome" type='submit'>
-                        Login for Admins
-                    </button>
+                            <button className="form-input-btnhome" type='submit'>
+                                Login for Admins
+                            </button>
                         </Link>
                         <Link to="/Loginexecutive">
-                    <button className="form-input-btnhome"  type='submit'>
-                        Login for Executives
-                    </button>
+                            <button className="form-input-btnhome" type='submit'>
+                                Login for Executives
+                            </button>
                         </Link>
                         <Link to="/Loginmanagement">
-                    <button className="form-input-btnhome"  type='submit'>
-                        Login for Management
-                    </button>
+                            <button className="form-input-btnhome" type='submit'>
+                                Login for Management
+                            </button>
                         </Link>
                     </div>
                 </div>
