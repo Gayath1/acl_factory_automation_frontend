375a3f9588e8988b3ab9b68514c02a3fa43ecb50
Gayath1 <chandulagayan@gmail.com>
2021-08-19 12:59:21 +0530

    Table filter for dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState,forwardRef } from 'react'
 
 import {Link, Route} from 'react-router-dom'
 import Sidebar from "../components/sidebar/Sidebar";
@@ -7,6 +7,42 @@ import StatusCard from '../components/status-card/StatusCard';
 import Chart from 'react-apexcharts'
 import Table from '../components/table/Table';
 import Badge from '../components/badge/Badge';
+import MaterialTable from "material-table";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 const chartOptions = {
     series: [{
@@ -155,6 +191,34 @@ const latestOrders = {
     ]
 }
 
+const rows = [
+    {
+        name: "Tomato",
+        color: "red",
+        quantity: 12,
+        id: "01"
+    },
+    {
+        name: "Banana",
+        color: "yellow",
+        quantity: 5,
+        id: "02"
+    },
+    {
+        name: "Lemon",
+        color: "yellow",
+        quantity: 20,
+        id: ""
+    },
+    {
+        name: "Blueberry",
+        color: "blue",
+        quantity: 50,
+        id: ""
+    }
+]
+
+
 const orderStatus = {
     "shipping": "primary",
     "pending": "warning",
@@ -178,11 +242,34 @@ const renderOrderBody = (item, index) => (
     </tr>
 )
 
+
+
 const Dashboard = () => {
 
+    const [data, setData] = useState(rows);
+    const [checked, setChecked] = useState(false);
 
+    const filterValue = value => {
+        if (value) {
+            const filtered = data.filter(d => d.id.trim().length > 0);
+            setData(filtered);
+        } else {
+            setData(rows);
+        }
+        setChecked(value);
+    };
 
-    return (
+    const columns = [
+        {
+            title: "Name",
+            field: "name",
+        },
+        { title: "Color", field: "color", filtering: false },
+        { title: "Quantity", field: "quantity", filtering: false },
+        { title: "ID", field: "id", filtering: false, hidden: true }
+    ];
+
+        return (
         <>
             <Sidebar/>
             <div id="main" className="layout__content">
@@ -252,6 +339,26 @@ const Dashboard = () => {
                                     </div>
                                     <div className="card__footer">
                                         <Link to='/'>view all</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-8">
+                                <div className="card">
+                                    <div className="card__header">
+                                        <h3>Machine Downtime reasons</h3>
+                                    </div>
+                                    <div className="card__body">
+                                        <MaterialTable
+                                            title=""
+                                            columns={columns}
+                                            data={data}
+                                            icons={tableIcons}
+                                            // options={{
+                                            //     filtering: true
+                                            // }}
+                                        />
                                     </div>
                                 </div>
                             </div>
