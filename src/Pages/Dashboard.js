f4a4b6d8b8b91b308220b1abea7a89681cb8c9b3
Gayath1 <chandulagayan@gmail.com>
2021-08-20 11:29:52 +0530

    Dashboard changes

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,6 @@
-import React, {useEffect,useState,forwardRef } from 'react'
-
-import {Link, Route} from 'react-router-dom'
+import React, {useEffect, useState, forwardRef} from 'react'
+import {makeStyles} from '@material-ui/core/styles';
+import {Link, Route} from 'react-router-dom';
 import Sidebar from "../components/sidebar/Sidebar";
 import TopNav from "../components/topnav/TopNav";
 import StatusCard from '../components/status-card/StatusCard';
@@ -23,31 +23,65 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Popover from '@material-ui/core/Popover';
+import Button from '@material-ui/core/Button';
 
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
 };
+
+function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 const chartOptions = {
     series: [{
         name: 'Online Customers',
-        data: [40,70,20,90,36,80,30,91,60]
+        data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
     }, {
         name: 'Store Customers',
         data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
@@ -74,28 +108,11 @@ const chartOptions = {
         }
     }
 }
-const statusCards = [
-    {
-        "icon": "bx bx-shopping-bag",
-        "count": "1,995",
-        "title": "Daily Availability"
-    },
-    {
-        "icon": "bx bx-cart",
-        "count": "2,001",
-        "title": "OEE"
-    },
-    {
-        "icon": "bx bx-dollar-circle",
-        "count": "$2,632",
-        "title": "OEEP"
-    },
-    {
-        "icon": "bx bx-receipt",
-        "count": "1,711",
-        "title": "Total Productivity"
-    }
-]
+
+
+
+
+
 
 const topCustomers = {
     head: [
@@ -242,12 +259,57 @@ const renderOrderBody = (item, index) => (
     </tr>
 )
 
-
+const useStyles = makeStyles((theme) => ({
+    typography: {
+        padding: theme.spacing(2),
+    },
+}));
 
 const Dashboard = () => {
 
+    const classes = useStyles();
     const [data, setData] = useState(rows);
     const [checked, setChecked] = useState(false);
+    const [value, setValue] = React.useState(0);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const statusCards = [
+        {
+            "icon": "bx bx-shopping-bag",
+            "count": "1,995",
+            "title": "Daily Availability"
+        },
+        {
+            "icon": "bx bx-cart",
+            "count": "2,001",
+            "title": "OEE"
+        },
+        {
+            "icon": "bx bx-dollar-circle",
+            "count": "$2,632",
+            "title": "OEEP"
+        },
+        {
+            "icon": "bx bx-wrench",
+            "count": "1,711",
+            "title": "Total Breakdown",
+            "onClick": handleClick
+        }
+    ]
+
+
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handletab = (event, newValue) => {
+        setValue(newValue);
+    };
 
     const filterValue = value => {
         if (value) {
@@ -264,17 +326,35 @@ const Dashboard = () => {
             title: "Name",
             field: "name",
         },
-        { title: "Color", field: "color", filtering: false },
-        { title: "Quantity", field: "quantity", filtering: false },
-        { title: "ID", field: "id", filtering: false, hidden: true }
+        {title: "Color", field: "color", filtering: false},
+        {title: "Quantity", field: "quantity", filtering: false},
+        {title: "ID", field: "id", filtering: false, hidden: true}
     ];
 
-        return (
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    return (
         <>
             <Sidebar/>
             <div id="main" className="layout__content">
                 <TopNav/>
                 <div className="layout__content-main">
+                    <AppBar position="static" style={{
+                        background: `linear-gradient(90deg, #06518C 0%, #62B4FF 97.85%)`,
+                        borderRadius: "8px"
+                    }}>
+                        <Tabs TabIndicatorProps={{
+                            style: {
+                                backgroundColor: "#ffffff"
+                            }
+                        }} value={value} onChange={handletab}>
+                            <Tab label="Dashboard" {...a11yProps(0)} />
+                            <Tab label="Pending" {...a11yProps(1)} />
+                        </Tabs>
+                    </AppBar>
+                </div>
+                <TabPanel value={value} index={0}>
                     <div>
                         <h2 className="page-header">Dashboard</h2>
                         <div className="row">
@@ -287,10 +367,35 @@ const Dashboard = () => {
                                                     icon={item.icon}
                                                     count={item.count}
                                                     title={item.title}
+                                                    onClick={item.onClick}
                                                 />
                                             </div>
                                         ))
                                     }
+                                    <Popover
+                                        id={id}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        onClose={handleClose}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'center',
+                                        }}
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'center',
+                                        }}
+                                    >
+                                        <MaterialTable
+                                            title=""
+                                            columns={columns}
+                                            data={data}
+                                            icons={tableIcons}
+                                            // options={{
+                                            //     filtering: true
+                                            // }}
+                                        />
+                                    </Popover>
                                 </div>
                             </div>
                             <div className="col-6">
@@ -364,10 +469,14 @@ const Dashboard = () => {
                             </div>
                         </div>
                     </div>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+
+                </TabPanel>
 
 
             </div>
-                </div>
+
         </>
     )
 }
