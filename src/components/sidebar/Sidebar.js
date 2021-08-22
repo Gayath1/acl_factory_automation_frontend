a12a3283effbe6d4d90d7ed78c5b90e1bc722206
Gayath1 <chandulagayan@gmail.com>
2021-08-22 12:35:08 +0530

    changes

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
-import React,{useState,useContext } from 'react'
+import React, {useState, useContext} from 'react'
 
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import UserContext from '../../userContext'
 import './sidebar.css'
 import logo from '../../assets/INNOVIGENT.png';
@@ -50,68 +50,67 @@ const sidebar_items1 = [
 ]
 
 const sidebar_items = [
-        {
-            "display_name": "Dashboard",
-            "route": "/Dashboard",
-            "icon": "bx bx-category-alt"
-        },
-        {
-            "display_name": "Admin",
-            "route": "/Admin",
-            "icon": "bx bx-user"
-        },
-        {
-            "display_name": "Users",
-            "route": "/Usercreate",
-            "icon": "bx bx-user-pin"
-        },
-        {
-            "display_name": "Shifts",
-            "route": "/Shift",
-            "icon": "bx bx-time-five"
-        },
-        {
-            "display_name": "Departments",
-            "route": "/Department",
-            "icon": "bx bx-building"
-        },
-        {
-            "display_name": "Factories",
-            "route": "/Factory",
-            "icon": "bx bxs-factory"
-        },
-        {
-            "display_name": "Fault Cases",
-            "route": "/SpecialCase",
-            "icon": "bx bx-import"
-        },
-        {
-            "display_name": "Fault Cases Email",
-            "route": "/SpecialCasesEmail",
-            "icon": "bx bx-briefcase-alt"
-        },
-        {
-            "display_name": "Product Line",
-            "route": "/ProductLine",
-            "icon": "bx bx-barcode"
-        },
-        {
-            "display_name": "Product Info",
-            "route": "/ProductInfo",
-            "icon": "bx bx-info-square"
-        },
-        {
-            "display_name": "Fault Manage",
-            "route": "/Fault",
-            "icon": "bx bx-error-alt"
-        },
-        {
-            "display_name": "settings",
-            "route": "/Settings",
-            "icon": "bx bx-cog"
-        }
-    ]
-
+    {
+        "display_name": "Dashboard",
+        "route": "/Dashboard",
+        "icon": "bx bx-category-alt"
+    },
+    {
+        "display_name": "Admin",
+        "route": "/Admin",
+        "icon": "bx bx-user"
+    },
+    {
+        "display_name": "Users",
+        "route": "/Usercreate",
+        "icon": "bx bx-user-pin"
+    },
+    {
+        "display_name": "Shifts",
+        "route": "/Shift",
+        "icon": "bx bx-time-five"
+    },
+    {
+        "display_name": "Departments",
+        "route": "/Department",
+        "icon": "bx bx-building"
+    },
+    {
+        "display_name": "Factories",
+        "route": "/Factory",
+        "icon": "bx bxs-factory"
+    },
+    {
+        "display_name": "Fault Cases",
+        "route": "/SpecialCase",
+        "icon": "bx bx-import"
+    },
+    {
+        "display_name": "Fault Cases Email",
+        "route": "/SpecialCasesEmail",
+        "icon": "bx bx-briefcase-alt"
+    },
+    {
+        "display_name": "Product Line",
+        "route": "/ProductLine",
+        "icon": "bx bx-barcode"
+    },
+    {
+        "display_name": "Product Info",
+        "route": "/ProductInfo",
+        "icon": "bx bx-info-square"
+    },
+    {
+        "display_name": "Fault Manage",
+        "route": "/Fault",
+        "icon": "bx bx-error-alt"
+    },
+    {
+        "display_name": "settings",
+        "route": "/Settings",
+        "icon": "bx bx-cog"
+    }
+]
 
 
 const SidebarItem = props => {
@@ -148,60 +147,65 @@ const Sidebar = props => {
         setIsCollapsed(false)
         document.getElementById("mySidebar").style.width = "300px";
         // document.getElementById("main").style.paddingLeft = "300px";
-        {window.matchMedia("(max-width: 800px)").matches?
-            (document.getElementById("main").style.paddingLeft = "80px"):
-            document.getElementById("main").style.paddingLeft = "300px";
+        {
+            window.matchMedia("(max-width: 800px)").matches ?
+                (document.getElementById("main").style.paddingLeft = "80px") :
+                document.getElementById("main").style.paddingLeft = "300px";
         }
     }
 
 
     return (
         <>
-        {userData.role === 1 || userData.role === 50? (
-        <div id="mySidebar" className='sidebar'>
-            {isCollapsed === true ? <div className="sidebar__itemmenu">
-                <button className="sidebar__item-inner"  onClick={openNav}><i className='bx bx-menu'></i></button>
-            </div>:
-                <div className="sidebar__itemmenu">
-                    <button className="sidebar__item-inner"  onClick={closeNav}><i className='bx bx-menu'></i></button>
-                </div>
-            }
-
-            <div className="sidebar__logo">
-                <img src={logo} alt="company logo" />
-            </div>
-
-            {
-                sidebar_items.map((item, index) => (
-                    <Link to={item.route} key={index}>
-                        {isCollapsed === true ?
-                            <SidebarItem
-                                icon={item.icon}
-                                active={index === activeItem}
-                            />:
-                            <SidebarItem
-                                title={item.display_name}
-                                icon={item.icon}
-                                active={index === activeItem}
-                            />
-                        }
-                    </Link>
-                ))
-            }
-        </div>
-    ):null}
-            {userData.role === 70  ?(
+            {userData.role === 1 || userData.role === 50 ? (
                 <div id="mySidebar" className='sidebar'>
                     {isCollapsed === true ? <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={openNav}><i className='bx bx-menu'></i></button>
-                        </div>:
+                            <button className="sidebar__item-inner" onClick={openNav}><i className='bx bx-menu'></i>
+                            </button>
+                        </div> :
                         <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={closeNav}><i className='bx bx-menu'></i></button>
+                            <button className="sidebar__item-inner" onClick={closeNav}><i className='bx bx-menu'></i>
+                            </button>
                         </div>
                     }
 
                     <div className="sidebar__logo">
-                        <img src={logo} alt="company logo" />
+                        <img src={logo} alt="company logo"/>
+                    </div>
+
+                    {
+                        sidebar_items.map((item, index) => (
+                            <Link to={item.route} key={index}>
+                                {isCollapsed === true ?
+                                    <SidebarItem
+                                        icon={item.icon}
+                                        active={index === activeItem}
+                                    /> :
+                                    <SidebarItem
+                                        title={item.display_name}
+                                        icon={item.icon}
+                                        active={index === activeItem}
+                                    />
+                                }
+                            </Link>
+                        ))
+                    }
+                </div>
+            ) : null}
+            {userData.role === 70 ? (
+                <div id="mySidebar" className='sidebar'>
+                    {isCollapsed === true ? <div className="sidebar__itemmenu">
+                            <button className="sidebar__item-inner" onClick={openNav}><i className='bx bx-menu'></i>
+                            </button>
+                        </div> :
+                        <div className="sidebar__itemmenu">
+                            <button className="sidebar__item-inner" onClick={closeNav}><i className='bx bx-menu'></i>
+                            </button>
+                        </div>
+                    }
+
+                    <div className="sidebar__logo">
+                        <img src={logo} alt="company logo"/>
                     </div>
 
                     {
@@ -211,7 +215,7 @@ const Sidebar = props => {
                                     <SidebarItem
                                         icon={item.icon}
                                         active={index === activeItem1}
-                                    />:
+                                    /> :
                                     <SidebarItem
                                         title={item.display_name}
                                         icon={item.icon}
@@ -222,19 +226,21 @@ const Sidebar = props => {
                         ))
                     }
                 </div>
-            ):null}
-            {userData.role === 71 || userData.role === 72 || userData.role === 30  ?(
+            ) : null}
+            {userData.role === 71 || userData.role === 72 || userData.role === 30 ? (
                 <div id="mySidebar" className='sidebar'>
                     {isCollapsed === true ? <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={openNav}><i className='bx bx-menu'></i></button>
-                        </div>:
+                            <button className="sidebar__item-inner" onClick={openNav}><i className='bx bx-menu'></i>
+                            </button>
+                        </div> :
                         <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={closeNav}><i className='bx bx-menu'></i></button>
+                            <button className="sidebar__item-inner" onClick={closeNav}><i className='bx bx-menu'></i>
+                            </button>
                         </div>
                     }
 
                     <div className="sidebar__logo">
-                        <img src={logo} alt="company logo" />
+                        <img src={logo} alt="company logo"/>
                     </div>
 
                     {
@@ -244,7 +250,7 @@ const Sidebar = props => {
                                     <SidebarItem
                                         icon={item.icon}
                                         active={index === activeItem2}
-                                    />:
+                                    /> :
                                     <SidebarItem
                                         title={item.display_name}
                                         icon={item.icon}
@@ -255,19 +261,21 @@ const Sidebar = props => {
                         ))
                     }
                 </div>
-            ):null}
-            {userData.role === 2 ?(
+            ) : null}
+            {userData.role === 2 ? (
                 <div id="mySidebar" className='sidebar'>
                     {isCollapsed === true ? <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={openNav}><i className='bx bx-menu'></i></button>
-                        </div>:
+                            <button className="sidebar__item-inner" onClick={openNav}><i className='bx bx-menu'></i>
+                            </button>
+                        </div> :
                         <div className="sidebar__itemmenu">
-                            <button className="sidebar__item-inner"  onClick={closeNav}><i className='bx bx-menu'></i></button>
+                            <button className="sidebar__item-inner" onClick={closeNav}><i className='bx bx-menu'></i>
+                            </button>
                         </div>
                     }
 
                     <div className="sidebar__logo">
-                        <img src={logo} alt="company logo" />
+                        <img src={logo} alt="company logo"/>
                     </div>
 
                     {
@@ -277,7 +285,7 @@ const Sidebar = props => {
                                     <SidebarItem
                                         icon={item.icon}
                                         active={index === activeItem3}
-                                    />:
+                                    /> :
                                     <SidebarItem
                                         title={item.display_name}
                                         icon={item.icon}
@@ -288,7 +296,7 @@ const Sidebar = props => {
                         ))
                     }
                 </div>
-            ):null}
+            ) : null}
         </>
     )
 }
